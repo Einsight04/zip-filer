56f9566e05ff91c607fdb000be98250df786d5e3
einsight <ghaithkhalil2004@gmail.com>
2023-03-01 21:31:37 -0500

    updates

---
+++ src/zip-folder-creator.ts
@@ -0,0 +1,27 @@
+import JSZip from 'jszip';
+import fs from "fs";
+import path from 'path';
+
+class ZipFolderCreator {
+    private files: { [name: string]: string } = {};
+
+    constructor(...fileContents: string[]) {
+        fileContents.forEach((content, index) => {
+            this.files[`file_${index}.txt`] = content;
+        });
+    }
+
+    async createZipFolder(outputPath: string): Promise<void> {
+        const zip = new JSZip();
+
+        Object.entries(this.files).forEach(([name, content]) => {
+            zip.file(name, content);
+        });
+
+        const content = await zip.generateAsync({ type: 'nodebuffer' });
+        const outputFilePath = path.resolve(outputPath);
+        fs.writeFileSync(outputFilePath, content);
+    }
+}
+
+export default ZipFolderCreator;
